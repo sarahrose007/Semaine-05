5c37e4c3c6b1ce5f179124a2506f35719487fbc9
sarahrose007 <66729669+sarahrose007@users.noreply.github.com>
2020-07-20 11:28:22 +0200

    COORECTION

--- Exercices/landing-page/Landing-page/script.js
+++ Exercices/landing-page/Landing-page/script.js
@@ -25,7 +25,3 @@ fetch("https://quotes-light-api.herokuapp.com/api/comments/", {
       //je spécifie à quel endroit de mon HTML je veux insérer ce contenu
     });
   })
-
-
-  <
-  https: //teams.microsoft.com/l/message/19:f53cd58085c14475ab3f9111fb531f23@thread.tacv2/1595234182074?tenantId=2933c53a-92a2-4dd4-8807-1ad266d35da5&amp;groupId=d94e8fff-ffc5-4ea6-9ace-1082c40f24a2&amp;parentMessageId=1595234182074&amp;teamName=SiriusSchool Formation&amp;channelName=Javascript&amp;createdTime=1595234182074>
